dfca3c082e49096cbadc4deccad1eff56fd84ba6
monkeymoves <lukejjmaggs@gmail.com>
2018-09-15 16:47:24 +0100

    map implementation and sample geoJson site information

--- data.js
+++ data.js
@@ -1,6 +1,490 @@
-let data = [{
+DSWsite_callback (
+    {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.254856, 51.651209]
+            },
+            "properties": {
+                "id": 2,
+              "name": "Parc Penallta",
+              "siteClass":"Mily Way",
+              "website": "http://your.caerphilly.gov.uk/countryside/country-parks/parc-penallta",
+              "description": "The Parc is an ideal location to observe the night sky. Situated near the car park is a playing field size area that affords excellent views to all horizons",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.4041536, 51.651871]
+            },
+            "properties": {
+                "id": 3,
+              "name": "Daerwynno Outdoor Centre",
+              "siteClass":"Mily Way",
+              "website": "http://www.daerwynno.org",
+              "description": "The site is located between the Rhondda and Cynon valleys and sits within a natural bowl. The site provides a wide panorama with slightly obscured views below 15 degrees.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.488388, 51.686505]
+            },
+            "properties": {
+                "id": 4,
+              "name": "Garn Eiddel Car Park",
+              "siteClass":"Mily Way",
+              "website": "https://www.facebook.com/darkskywales/",
+              "description": "Garn Eiddel Car Park is located on top of Maerdy mountain. The site provides an excellent panorama. Some slight light pollution to southerly aspect",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.5406143, 51.647160]
+            },
+            "properties": {
+                "id": 5,
+              "name": "Bwlch Mountain Lay-by",
+              "siteClass":"Mily Way",
+              "website": "https://www.facebook.com/darkskywales/",
+              "description": "A lay-by located on top of the Bwlch mountain the site provides uninterrupted views to the South, East and West",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.5599408, 51.706502]
+            },
+            "properties": {
+              "id": 6,
+              "name": "Hendre Mynydd Car Park",
+              "siteClass":"Mily Way",
+              "website": "https://www.facebook.com/darkskywales/",
+              "description": "Hendre Mynydd Car Park is located on top of the Rhigos mountain the site provides uninterrupted views to the South, East and West.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.631795, 51.673858]
+            },
+            "properties": {
+              "id": 7,
+              "name": "Glyncorrwg Ponds",
+              "siteClass":"Mily Way",
+              "website": "http://glyncorrwgpondsvisitorcentre.co.uk",
+              "description": "The ponds are situated at the beginning of the Glyncorrwg village and provide splendid unobstructed views of the night sky",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.5297762, 51.765617]
+            },
+            "properties": {
+              "id": 8,
+              "name": "Red Lion Pub Penderyn",
+              "siteClass":"Mily Way",
+              "website": "http://www.redlionpenderyn.com/",
+              "description": "The Red Lion Pub is located near the village of Penderyn within the Brecon Beacons National Park",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.981311, 51.749066]
+            },
+            "properties": {
+              "id": 9,
+              "name": "Gerazim Chapel",
+              "siteClass":"Mily Way",
+              "website": "https://www.facebook.com/darkskywales/",
+              "description": "The Chapel is located on top of a hill near the village of Cwmcerdenin and provides amazing unobstructed views of all horizons.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.3680940,  51.820257]
+            },
+            "properties": {
+              "id": 10,
+              "name": "Plas Dolygaer",
+              "siteClass":"Mily Way",
+              "website": "https://www.parkwoodoutdoors.co.uk/centre/dolygaer",
+              "description": "Group Indoor Accommodation Centre and campsite located in the Brecon Beacons Reserve that is not directly overlooked by any street lights.",
+              "events": "" 
+            }
+          },
+            {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.6962575, 51.942764]
+            },
+            "properties": {
+              "id": 11,
+              "name": "Trecastle Usk Reservoir Car Park",
+              "siteClass":"Mily Way",
+              "website": "https://www.facebook.com/darkskywales/",
+              "description": "Remote location in the heart of the International Dark Sky Reserve, with excellent access and within short distance of Trecastle..",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.604692, 51.909098]
+            },
+            "properties": {
+              "id": 12,
+              "name": "Crai Village",
+              "siteClass":"Mily Way",
+              "website": "https://www.facebook.com/darkskywales/",
+              "description": "The village is set in a rural location and offers unhindered views of all horizons with minamal localised lighting.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.2905140, 51.593977]
+            },
+            "properties": {
+              "id": 13,
+              "name": "Rose and Crown Public House Eglwsilan",
+              "siteClass":"Orion",
+              "website": "http://www.sabrain.com/rose-and-crown-abertridwr",
+              "description": "The site is located above the Pontypridd area and has fantastic panorama of the South Wales Valleys extending to the Severn Estuary",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.370080, 51.609103]
+            },
+            "properties": {
+              "id": 14,
+              "name": "Barry Sidings",
+              "siteClass":"Orion",
+              "website": "http://www.rctcbc.gov.uk/en/leisurelibrariesculture/parksandcountryside/parks-informationandadvice/barrysidings/barry-sidings.aspx",
+              "description": "The site has ample parking facilities outside of car park at night and is easily located between the villages of Hopkinstown and Trehafod. The park provides good southerly views but limited towards western and eastern horizons",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.47134, 51.711927]
+            },
+            "properties": {
+              "id": 15,
+              "name": "Dare Valley Country Park",
+              "siteClass":"Orion",
+              "website": "http://www.darevalleycountrypark.co.uk/index.php/en/",
+              "description": "Dare Valley Parc has ample parking and a large area in which to observe the night sky. Good horizons with slightly obstructed view to the west.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.48907100908571, 51.9248876779592]
+            },
+            "properties": {
+              "id": 16,
+              "name": "Mountain Centre, Libanus",
+              "siteClass":"Milky Way/Orion ",
+              "website": "http://www.brecon-beacons.com/mountain-centre.html",
+              "description": "A public area near the town of Libanus. The site is dark with very little light pollution from the surrounding area.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-4.151505, 51.839866]
+            },
+            "properties": {
+              "id": 17,
+              "name": "National Botanical Garden of Wales",
+              "siteClass":"Mily Way",
+              "website": "http://www.gardenofwales.org.uk",
+              "description": "The National Botanic Garden of Wales is set on historic parkland, dating back around 400 years.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-4.7725123, 51.643140]
+            },
+            "properties": {
+              "id": 18,
+              "name": "Skrinkle Haven car park",
+              "siteClass":"Mily Way",
+              "website": "http://www.pembrokeshirecoast.org.uk",
+              "description": "The National Park Authority parking area at Skrinkle Haven is an unlit cliff-top site with very good visibility in all directions.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-4.922928, 51.607498]
+            },
+            "properties": {
+              "id": 19,
+              "name": "Broad Haven South Carpark",
+              "siteClass":"Mily Way",
+              "website": "http://www.nationaltrust.org.uk/stackpole-centre",
+              "description": "Uncluttered, stunning cliff top location at the heart of the National Park, offering 360 degree panorama of the night sky.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-5.1799689, 51.694334]
+            },
+            "properties": {
+              "id": 20,
+              "name": "Kete National Trust car park",
+              "siteClass":"Mily Way",
+              "website": "www.nationaltrust.org.uk/marloes-sands",
+              "description": "Large car park with firm surface and superb uninterrupted views to the South, West and North completely free from skyglow",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-5.2426775, 51.734739]
+            },
+            "properties": {
+              "id": 21,
+              "name": "Martins Haven National Trust car park",
+              "siteClass":"Mily Way",
+              "website": "www.nationaltrust.org.uk/marloes-sands",
+              "description": "A large car park with good all round visibility, enjoying very dark skies with little local light pollution",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-5.1175206, 51.846760]
+            },
+            "properties": {
+              "id": 22,
+              "name": "Newgale Beach (mid/southern end)",
+              "siteClass":"Mily Way",
+              "website": "https://goo.gl/maps/LNltA",
+              "description": "Newgale Sands is a long WSW-facing beach. The recommended location is on the beach, off the Pebbles car park, however the beach is unsuitable for less able users; the carpark affords an alternative viewing site",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-5.0625206, 52.008082]
+            },
+            "properties": {
+              "id": 23,
+              "name": "Garn Fawr National Trust car park",
+              "siteClass":"Mily Way",
+              "website": "www.nationaltrust.org.uk/strumble-head-cardigan",
+              "description": "A small car park amongst rocky outcrops with good views of sky and little local light pollution.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-4.8472218, 51.979078]
+            },
+            "properties": {
+              "id": 24,
+              "name": "Sychpant PCNPA picnic site",
+              "siteClass":"Mily Way",
+              "website": "http://www.pembrokeshirecoast.org.uk",
+              "description": "Very good dark skies with little light pollution in a valley location",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-4.6992002, 52.104162]
+            },
+            "properties": {
+              "id": 25,
+              "name": "Poppit Sands beach serviced by Poppit Sands PCNPA car park",
+              "siteClass":"Mily Way",
+              "website": "https://goo.gl/maps/pUwNA",
+              "description": "The least restricted views of the sky from Poppit Sands are from the beach (soft sand) accessed by crossing a quiet road to a wooden boardwalk immediately west of the RNLI lifeboat station",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-4.4959917, 52.143629]
+            },
+            "properties": {
+              "id": 26,
+              "name": "Penbryn, Ceredigio",
+              "siteClass":"Mily Way",
+              "website": "http://www.nationaltrust.org.uk/llanerchaeron",
+              "description": "National Trust car park next to beach.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-4.2264775, 52.218981]
+            },
+            "properties": {
+              "id": 27,
+              "name": "Llanerchaeron",
+              "siteClass":"Mily Way",
+              "website": "http://www.nationaltrust.org.uk/llanerchaeron",
+              "description": "National Trust car park which has no street lighting.",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.8275442, 52.165592]
+            },
+            "properties": {
+              "id": 28,
+              "name": "Tyn Cornel Hostel",
+              "siteClass":"Mily Way",
+              "website": "http://www.yha.org.uk",
+              "description": "YHA in a Old stonebuilt farmhouse with parking",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.7481231, 52.191073]
+            },
+            "properties": {
+              "id": 29,
+              "name": "Dolgoch Hostel",
+              "siteClass":"Mily Way",
+              "website": "http://www.yha.org.uk",
+              "description": "YHA in a Old stonebuilt farmhouse with parking",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.572181, 52.269407]
+            },
+            "properties": {
+              "id": 30,
+              "name": "Elan Valley",
+              "siteClass":"International Dark Sky Reserve",
+              "website": "http://www.elanvalley.org.uk/explore/dark-skies",
+              "description": "One of the very best sites in the UK let alone Wales",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.403244, 51.883273]
+            },
+            "properties": {
+              "id": 31,
+              "name": "Brecon Beacons Dark Sky Reserve",
+              "siteClass":"International Dark Sky Reserve",
+              "website": "http://www.beacons-npa.gov.uk/visit-us/outdoors-activities/star-gazing-and-dark-skies",
+              "description": "One of the very best sites in the UK let alone Wales",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-3.937244, 52.852518]
+            },
+            "properties": {
+              "id": 32,
+              "name": "Snowdonia Beacons Dark Sky Reserve",
+              "siteClass":"International Dark Sky Reserve",
+              "website": "http://www.eryri-npa.gov.uk/looking-after/dark-skies",
+              "description": "One of the very best sites in the UK let alone Wales",
+              "events": "" 
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-4.346264, 53.184112]
+            },
+            "properties": {
+              "id": 33,
+              "name": "Glan Morfa Lodge and Wildlife Experience",
+              "siteClass":"Mily Way",
+              "website": "https://www.holidaycottages-anglesey.com/",
+              "description": "",
+              "events": "" 
+            }
+          },
 
 
-}]
 
-let APIKEY = AIzaSyBIIvoo3p_X28uIYfkETl7-kZkLH7p5cRA
+        ]
+      }
+    )
